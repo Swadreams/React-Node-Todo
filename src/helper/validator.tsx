e69c9764d9d40b89197bae1be7c8e1a0160fbe4f
schute <schute@mobiquityinc.com>
2021-06-30 15:38:37 +0530

    Removed outer repository files

--- src/helper/validator.tsx
+++
@@ -1,6 +0,0 @@
-import validator from 'validator';
-
-export const validateEmail = (email: string) => {
-  const valid = validator.isEmail(email);
-  return valid ? true : false;
-};
--- src/redux/urls.tsx
+++
@@ -1,6 +0,0 @@
-export const BASE_URL = 'https://todo-api-v2.vercel.app';
-export const BASE_URL1 = 'http://localhost:7000';
-export const LOGIN_URL = '/user/login';
-export const SIGNUP_URL = '/user/register';
-export const TODOS_URL = '/todos';
-export const CREATE_TODO_URL = '/todo';
